470a93d4c345c3b3284953e687c4e35e1c600088
MatBed <mateuszbedla@gmail.com>
2022-10-08 11:52:10 +0200

    [GRPC] add a project that uses grpc

---
+++ GRPC/GrpcClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using GrpcServer.Protos;
+
+var input = new HelloRequest { Name = "Mateusz" };
+
+var channel = GrpcChannel.ForAddress("https://localhost:7168");
+//var client = new Greeter.GreeterClient(channel);
+var client = new Customer.CustomerClient(channel);
+//var reply = await client.SayHelloAsync(input);
+
+var clientRequested = new CustomerLookupModel { UserId = 1 };
+
+var reply = await client.GetCustomerInfoAsync(clientRequested);
+
+Console.WriteLine($"{reply.FirstName} {reply.LastName}");
+using (var call = client.GetNewCustomer(new NewCustomerRequest()))
+{
+    while (await call.ResponseStream.MoveNext(CancellationToken.None))
+    {
+        var currentCustomer = call.ResponseStream.Current;
+        Console.WriteLine($"{currentCustomer.FirstName} {currentCustomer.LastName} {currentCustomer.Age} {currentCustomer.EmailAddress}");
+    }
+}
+
+    Console.ReadLine();
---
+++ GRPC/GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,57 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+
+namespace GrpcServer.Services;
+
+public class CustomerService : Customer.CustomerBase
+{
+    private readonly ILogger<CustomerService> _logger;
+
+    public CustomerService(ILogger<CustomerService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<CustomerModel> GetCustomerInfo(
+        CustomerLookupModel request, 
+        ServerCallContext context)
+    {
+        CustomerModel output = new ();
+        
+        if(request.UserId == 1)
+        {
+            output.FirstName = "Jacek";
+            output.LastName = "Kowalski";
+        }
+        else
+        {
+            output.FirstName = "Tomasz";
+            output.LastName = "Nowak";
+        }
+
+        return Task.FromResult(output);
+    }
+
+    public override async Task GetNewCustomer(
+        NewCustomerRequest request, 
+        IServerStreamWriter<CustomerModel> responseStream, 
+        ServerCallContext context)
+    {
+        var customers = new List<CustomerModel>
+        {
+            new CustomerModel
+            {
+                FirstName = "Janek",
+                LastName = "Kamionka",
+                Age = 32,
+                EmailAddress = "JKamionka@wp.pl",
+                IsAlive = true
+            }
+        };
+
+        foreach (var customer in customers)
+        {
+            await responseStream.WriteAsync(customer);
+        }
+    }
+}
