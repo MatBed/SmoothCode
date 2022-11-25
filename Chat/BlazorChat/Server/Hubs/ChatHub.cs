2f5e23f935f983895e2487f5ed188a5463d90e2f
MatBed <mateuszbedla@gmail.com>
2022-11-25 12:10:04 +0100

    [BlazorChat] add simple chat applicattion using SignalR

---
+++ Chat/BlazorChat/Server/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChat.Server.Hubs;
+
+public class ChatHub : Hub
+{ 
+    private static Dictionary<string, string> Users = new Dictionary<string, string>();
+
+    public override async Task OnConnectedAsync()
+    {
+        string username = Context.GetHttpContext().Request.Query["username"];
+        Users.Add(Context.ConnectionId, username);
+        await AddMessageToChat(string.Empty, $"{username} connected!");
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? ex)
+    {
+        string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+        await AddMessageToChat(string.Empty, $"{username} left!");
+    }
+
+    public async Task AddMessageToChat(string user, string message)
+    {
+        await Clients.All.SendAsync("GetThatMessageDude", user, message);
+    }
+}
