479d11f37d2c5cf432f951ddccf713a5eec68ac1
MatBed <mateuszbedla@gmail.com>
2023-02-13 14:55:10 +0100

    [HRLeaveManagement] create persistence project, implement repos

---
+++ HRLeaveManagement/Source/HR.LeaveManagement.Persistance/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Core;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR.LeaveManagement.Persistence.Repositories;
+public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+{
+    public LeaveTypeRepository(HrDatabaseContext context) : base(context)
+    {
+    }
+
+    public async Task<bool> IfLeaveTypeUnique(string name)
+    {
+        return await _context.LeaveTypes.AnyAsync(q => q.Name == name);
+    }
+}
