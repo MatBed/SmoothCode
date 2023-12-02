039a15f234afad6e6990a17baa1a8ceb18555ad3
MatBed <mateuszbedla@gmail.com>
2023-12-02 23:58:03 +0100

    update packeages and fix tests

--- HRLeaveManagement/Test/HR.LeaveManagement.Persistence.IntegrationTests/HrDatabaseContextTests.cs
+++ HRLeaveManagement/Test/HR.LeaveManagement.Persistence.IntegrationTests/HrDatabaseContextTests.cs
@@ -1,25 +1,26 @@
-﻿using HR.LeaveManagement.Core;
+﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Core;
 using HR.LeaveManagement.Persistence.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Persistence.IntegrationTests;
 
 public class HrDatabaseContextTests
 {
     private HrDatabaseContext _hrDatabaseContext;
-
+    private readonly string _userId;
+    private readonly Mock<IUserService> _userServiceMock;
     public HrDatabaseContextTests()
     {
         var dbOptions = new DbContextOptionsBuilder<HrDatabaseContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+        _userId = "00000000-0000-0000-0000-000000000000";
+        _userServiceMock = new Mock<IUserService>();
+        _userServiceMock.Setup(m => m.UserId).Returns(_userId);
 
-        _hrDatabaseContext = new HrDatabaseContext(dbOptions);
+        _hrDatabaseContext = new HrDatabaseContext(dbOptions, _userServiceMock.Object);
     }
 
     [Fact]
